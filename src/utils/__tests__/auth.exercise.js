6dab12ab25a24c8910ba126870a8fe6e51101d54
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-05-08 18:42:38 +0300

    improve error messages by generating test titles

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,21 +1,19 @@
 import {isPasswordAllowed} from '../auth'
 
-test('isPasswordAllowed returns false for invalid passwords', () => {
-  const shortPassword = 'a2c!'
-  const noAlphabetCharPwd = '123456!'
-  const noNumberPwd = 'ABCdef!'
-  const noUpperCasePwd = 'abc123!'
-  const noLowerCasePwd = 'ABC123!'
-  const noNonAlphanumericPwd = 'ABCdef123'
+const tests = [
+  {input: '!aBc123', output: true, message: 'valid'},
+  {input: 'a2C!', output: false, message: 'short'},
+  {input: '123456!', output: false, message: 'no alphabet char'},
+  {input: 'ABCdef!', output: false, message: 'no number'},
+  {input: 'abc123!', output: false, message: 'no uppercase'},
+  {input: 'ABC123!', output: false, message: 'no lowercase'},
+  {input: 'ABCdef123', output: false, message: 'no non-alphanumeric'},
+]
 
-  expect(isPasswordAllowed(shortPassword)).toBe(false)
-  expect(isPasswordAllowed(noAlphabetCharPwd)).toBe(false)
-  expect(isPasswordAllowed(noNumberPwd)).toBe(false)
-  expect(isPasswordAllowed(noUpperCasePwd)).toBe(false)
-  expect(isPasswordAllowed(noLowerCasePwd)).toBe(false)
-  expect(isPasswordAllowed(noNonAlphanumericPwd)).toBe(false)
-})
-
-test('isPasswordAllowed returns true for valid passwords', () => {
-  expect(isPasswordAllowed('!aBc123')).toBe(true)
+describe('isPasswordAllowed only allows some passwords', () => {
+  tests.forEach(({input, output, message}) => {
+    test(`isPasswordAllowed returns ${output} for ${message} passwords`, () => {
+      expect(isPasswordAllowed(input)).toBe(output)
+    })
+  })
 })
