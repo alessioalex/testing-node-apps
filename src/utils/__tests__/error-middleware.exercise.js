1b2f1f42a7811f10c3d56226af247b907fc47ee3
Alexandru Vladutu <github@social.alessioalex.com>
2020-05-12 00:48:05 +0300

    use utils/generate prebuilt test factory for error middleware

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -2,33 +2,18 @@
 
 // 🐨 you'll need both of these:
 import {UnauthorizedError} from 'express-jwt'
+import {buildReq, buildRes, buildNext} from 'utils/generate'
 import errorMiddleware from '../error-middleware'
-
-function getTestObjects({
-  headersSent = false,
-  error = new Error('sample error'),
-  ...overrides
-} = {}) {
-  const res = {
-    headersSent,
-    json: jest.fn(() => res),
-    status: jest.fn(() => res),
-  }
-  const req = {}
-  const next = jest.fn()
-
-  return {error, req, res, next, ...overrides}
-}
 
 // 🐨 Write a test for the UnauthorizedError case
 test('errorMiddleware responds with 401 code and message for unauthorized', () => {
-  const {error, req, res, next} = getTestObjects({
-    error: new UnauthorizedError('some_error_code', {
-      message: 'Some message',
-    }),
+  const error = new UnauthorizedError('some_error_code', {
+    message: 'Some message',
   })
+  const res = buildRes()
+  const next = buildNext()
 
-  errorMiddleware(error, req, res, next)
+  errorMiddleware(error, buildReq(), res, next)
 
   expect(res.status).toHaveBeenCalledWith(401)
   expect(res.status).toHaveBeenCalledTimes(1)
@@ -44,9 +29,11 @@ test('errorMiddleware responds with 401 code and message for unauthorized', () =
 
 // 🐨 Write a test for the headersSent case
 test('errorMiddleware delegates to next if headers already sent', () => {
-  const {error, req, res, next} = getTestObjects({headersSent: true})
+  const error = new Error('sample error')
+  const res = buildRes({headersSent: true})
+  const next = buildNext()
 
-  errorMiddleware(error, req, res, next)
+  errorMiddleware(error, buildReq(), res, next)
 
   expect(next).toHaveBeenCalledWith(error)
   expect(next).toHaveBeenCalledTimes(1)
@@ -57,9 +44,12 @@ test('errorMiddleware delegates to next if headers already sent', () => {
 
 // 🐨 Write a test for the else case (responds with a 500)
 test('errorMiddleware responds with 500 for other cases', () => {
-  const {error, req, res, next} = getTestObjects()
+  const error = new Error('sample error')
 
-  errorMiddleware(error, req, res, next)
+  const res = buildRes()
+  const next = buildNext()
+
+  errorMiddleware(error, buildReq(), res, next)
 
   expect(res.status).toHaveBeenCalledWith(500)
   expect(res.status).toHaveBeenCalledTimes(1)
