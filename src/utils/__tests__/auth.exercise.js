6815c62f2f87ad8abc1b1644681188567ed25d85
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-05-11 19:01:39 +0300

    create a casify function to generate cases for jest in case

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,16 +1,23 @@
 import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
 
+function casify(obj) {
+  return Object.entries(obj).map(([name, password]) => {
+    return {
+      name: `${password} - ${name}`,
+      password,
+    }
+  })
+}
+
 cases(
   'isPasswordAllowed: valid passwords',
   (options) => {
     expect(isPasswordAllowed(options.password)).toBe(true)
   },
-  {
-    'valid password': {
-      password: '!aBc123',
-    },
-  },
+  casify({
+    'valid password': '!aBc123',
+  }),
 )
 
 cases(
@@ -18,33 +25,9 @@ cases(
   (options) => {
     expect(isPasswordAllowed(options.password)).toBe(false)
   },
-  {
-    'too short': {
-      password: 'a2C!',
-    },
-    'no letters': {
-      password: '12345678!',
-    },
-    'no numbers': {
-      password: 'ABCdef!',
-    },
-  },
+  casify({
+    'too short': 'a2C!',
+    'no letters': '12345678!',
+    'no numbers': 'ABCdef!',
+  }),
 )
-
-const tests = [
-  {input: '!aBc123', output: true, message: 'valid'},
-  {input: 'a2C!', output: false, message: 'short'},
-  {input: '123456!', output: false, message: 'no alphabet char'},
-  {input: 'ABCdef!', output: false, message: 'no number'},
-  {input: 'abc123!', output: false, message: 'no uppercase'},
-  {input: 'ABC123!', output: false, message: 'no lowercase'},
-  {input: 'ABCdef123', output: false, message: 'no non-alphanumeric'},
-]
-
-describe('isPasswordAllowed only allows some passwords', () => {
-  tests.forEach(({input, output, message}) => {
-    test(`isPasswordAllowed returns ${output} for ${message} passwords`, () => {
-      expect(isPasswordAllowed(input)).toBe(output)
-    })
-  })
-})
