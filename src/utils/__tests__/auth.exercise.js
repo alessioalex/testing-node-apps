a8e6d92c2d5ed34ac3046b7c7b2604feef04d537
Alexandru Vladutu <alexandru.vladutu@gmail.com>
2020-05-11 18:49:32 +0300

    use jest-in-case to reduce duplication

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,4 +1,35 @@
+import cases from 'jest-in-case'
 import {isPasswordAllowed} from '../auth'
+
+cases(
+  'isPasswordAllowed: valid passwords',
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(true)
+  },
+  {
+    'valid password': {
+      password: '!aBc123',
+    },
+  },
+)
+
+cases(
+  'isPasswordAllowed: invalid passwords',
+  (options) => {
+    expect(isPasswordAllowed(options.password)).toBe(false)
+  },
+  {
+    'too short': {
+      password: 'a2C!',
+    },
+    'no letters': {
+      password: '12345678!',
+    },
+    'no numbers': {
+      password: 'ABCdef!',
+    },
+  },
+)
 
 const tests = [
   {input: '!aBc123', output: true, message: 'valid'},
